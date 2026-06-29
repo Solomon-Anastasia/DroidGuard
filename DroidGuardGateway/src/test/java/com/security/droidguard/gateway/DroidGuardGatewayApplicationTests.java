01836a2d95668b1b2ee10c678490e315a2f77b3a
Solomon-Anastasia <nasteasolomon1@gmail.com>
2026-06-29 13:14:16 +0300

    Creating the gateway app

---
+++ DroidGuardGateway/src/test/java/com/security/droidguard/gateway/DroidGuardGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.droidguard.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroidGuardGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
